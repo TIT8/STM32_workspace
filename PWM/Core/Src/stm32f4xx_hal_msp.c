485801eeb9c8a5e60667d44623f533ed63bdb133
Lorenzo Perlo <68781644+TIT8@users.noreply.github.com>
2024-10-15 19:51:22 +0200

    Sample of Trevi code

--- PWM/Core/Src/stm32f4xx_hal_msp.c
+++ PWM/Core/Src/stm32f4xx_hal_msp.c
@@ -1,3 +1,4 @@
+
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
@@ -120,6 +121,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
   /* USER CODE BEGIN TIM1_MspInit 1 */
 
   /* USER CODE END TIM1_MspInit 1 */
+
   }
 
 }
@@ -207,6 +209,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
   /* USER CODE BEGIN USART2_MspInit 1 */
 
   /* USER CODE END USART2_MspInit 1 */
+
   }
 
 }
